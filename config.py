652fc5f7ff094664d8f4b78383e924582ca8b15e
cookies2002 <cookies2002boy@gmail.com>
2025-04-07 09:43:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ HEROKU_API_KEY = "HRKU-852706f4-8cba-4108-912f-e6b0f4a788a8"
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 
-API_URL = getenv("API_URL", 'https://api.nexgenbots.in') #youtube song url
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 API_KEY = getenv("API_KEY", '30DxNexGenBotsYwAcdy') # youtube song api key, get it from https://t.me/RahulTC
 
 UPSTREAM_REPO = getenv(
