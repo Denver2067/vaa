a39b9127080b81f772acc2c3ef9ff6516060d042
Denver2067 <vellina095@gmail.com>
2025-04-13 16:30:39 +0530

    Update bot.py

--- PURVIMUSIC/core/bot.py
+++ PURVIMUSIC/core/bot.py
@@ -1,25 +1,26 @@
+import os
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
-
-import config
 
 from ..logging import LOGGER
 
 
 class PURVI(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info("Starting Bot...")
+
         super().__init__(
             name="PURVIMUSIC",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
+            api_id=int(os.getenv("API_ID")),
+            api_hash=os.getenv("API_HASH"),
+            bot_token=os.getenv("BOT_TOKEN"),
             in_memory=True,
             max_concurrent_transmissions=7,
         )
 
     async def start(self):
         await super().start()
+
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
@@ -27,19 +28,23 @@ class PURVI(Client):
 
         try:
             await self.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                chat_id=int(os.getenv("LOGGER_ID")),
+                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b></u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                parse_mode=ParseMode.HTML,
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+                "Bot failed to access the log group/channel. Make sure your bot is added to the group."
             )
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
+
+        a = await self.get_chat_member(int(os.getenv("LOGGER_ID")), self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
+                "Please promote your bot to admin in the log group/channel."
             )
+
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
         await super().stop()
+        
